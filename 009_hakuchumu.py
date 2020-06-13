8938afe0582d162e88e4278462dec7d2254d9f40
shusuke0812 <shu.ota0812@gmail.com>
2020-06-13 12:52:51 +0900

    [add]解答を追加（WA, RE）

--- 009_hakuchumu.py
+++ 009_hakuchumu.py
@@ -1,6 +1,7 @@
 """
 英小文字からなる文字列sがある。
-tが空文字列である状態から始め以下の操作を好きな回数繰り返すことでs=tとすることができるか判定する
+tが空文字列である状態から始め、以下の操作を好きな回数繰り返すことで
+s=tとすることができるか判定する
  
  　tの末尾に'dream' 'dreamer' 'erase' 'eraser'のいずれかを追加
 
@@ -16,4 +17,61 @@ s=tとすることができる場合、'YES'
 そうでない場合 'NO'
 """
 
+# TODO : RE, WAとなるのでどこがおかしいかデバッグする
+
+s = list(input())
+# s = list('erasedream')
+s.reverse()
+
+s_len = len(s)
+
+flag = 'NO'
+
+t = []
+t_add = ['dream', 'dreamer', 'erase', 'eraser']
+t_rev_add = ['maerd', 'remaerd', 'esare', 'resare']
+
+# 方針：入力値の最後尾文字からスライスして文字列マッチングを行う
+
+de = 4 # dream or erase の文字数
+dr = 6 # dreamer の文字数
+er = 5 # eraser の文字数
+
+while True:
+    if s[0] == 'm':
+        if s[4] == 'd' or s[4] == 'e':
+            flag = 'YES'
+            del s[0:de+1]
+        else:
+            flag = 'NO'
+            break
+
+    elif s[0] == 'e':
+        if s[4] == 'e':
+            flag = 'YES'
+            del s[0:de+1]
+        else:
+            flag = 'NO'
+            break
+
+    elif s[0] == 'r':
+        if s[6] == 'd':
+            flag = 'YES'
+            del s[0:dr+1]
+        elif s[5] == 'e':
+            flag = 'YES'
+            del s[0:er+1]
+        else:
+            flag = 'NO'
+            break
+
+    # print('s: ', s)
+    # print('flag: ', flag)
+
+    if len(s) <= 0:
+        break
+
+print(flag)
+
+        
 
