3f14cfa62fc760c50124ec911a13c0599e14328b
shusuke0812 <shu.ota0812@gmail.com>
2020-06-25 13:08:24 +0900

    [add]解答を追加（一部TLE）

--- 012_sum_of_three_integers.py
+++ 012_sum_of_three_integers.py
@@ -14,3 +14,25 @@ k s
 出力
 問題の条件を満たすx,y,zの組が何通りあるか出力する
 """
+
+k, s = map(int,input().split())
+# print(k)
+# print(s)
+
+# 組み合わせをカウントする変数
+count = 0
+
+# 変数の初期化
+x = 0
+y = 0
+z = 0
+
+for x in range(k+1):
+    for y in range(k+1):
+        for z in range(k+1):
+            if x + y + z == s:
+                count += 1
+
+print(count)
+
+
