40a21dada62602bd549ba77c8e3465ed6f580d0c
shusuke0812 <shu.ota0812@gmail.com>
2020-06-28 09:04:17 +0900

    [fix]解答を追加

--- 013_back_and_forth.py
+++ 013_back_and_forth.py
@@ -36,4 +36,46 @@ for i in range(len(coordinates)):
 # print(s)
 # print(t)
 
+dx = t[0] - s[0]
+dy = t[1] - s[1]
 
+route = ""
+
+# print("dx: ", dx)
+# print("dy: ", dy)
+
+# 経路パターン１
+# 行き
+for i in range(dx):
+    route += "R"
+for i in range(dy):
+    route += "U"
+
+# 帰り
+for i in range(dx):
+    route += "L"
+for i in range(dy):
+    route += "D"
+
+# 経路パターン２（パターン１の外側を通る）
+# 行き
+route += "D"
+
+for i in range(dx+1):
+    route += "R"
+for j in range(dy+1):
+    route += "U"
+
+route += "L"
+route += "U"
+
+for i in range(dx+1):
+    route += "L"
+for i in range(dy+1):
+    route += "D"
+
+route += "R"
+
+
+# 解答出力
+print(route)
