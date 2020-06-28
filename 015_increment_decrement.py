a600f265ccc759b3b64d25429260aabf24bcf86b
shusuke0812 <shu.ota0812@gmail.com>
2020-06-28 10:07:25 +0900

    [add]解答を追加

--- 015_increment_decrement.py
+++ 015_increment_decrement.py
@@ -30,3 +30,13 @@ for i in s:
 
 # print(s_list)
 
+for i in s_list:
+    if i == 'I':
+        x += 1
+        if x > x_max:
+            x_max = x
+    elif i == 'D':
+        x -= 1
+
+
+print(x_max)
