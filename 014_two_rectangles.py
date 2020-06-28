0776afa13a89c99ff9788e5f35573c0c5c34a0d7
shusuke0812 <shu.ota0812@gmail.com>
2020-06-28 09:33:34 +0900

    [add]解答を追加

--- 014_two_rectangles.py
+++ 014_two_rectangles.py
@@ -19,3 +19,13 @@ A B C D
 a,b,c,d = map(int, input().split())
 
 # print(a,b,c,d)
+
+rectangle_x = a * b
+rectangle_y = c * d
+
+if rectangle_x > rectangle_y:
+    print(rectangle_x)
+elif rectangle_x < rectangle_y:
+    print(rectangle_y)
+else:
+    print(rectangle_x)
