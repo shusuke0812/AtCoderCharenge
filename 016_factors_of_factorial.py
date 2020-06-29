6f794017b9a99a0313a49c144df75a264f112741
shusuke0812 <shu.ota0812@gmail.com>
2020-06-29 22:20:19 +0900

    [add]解答を追加（TLE）

--- 016_factors_of_factorial.py
+++ 016_factors_of_factorial.py
@@ -13,10 +13,22 @@ n!の正の約数の個数を10^9+7で割った余り
 """
 
 n = int(input())
-ans = 1
+
+factorial = 1
 divisor = []
+ans = 0
 
 for i in range(1,n+1):
-    ans = ans * i
+    factorial = factorial * i
 
+# print(factorial)
 
+for i in range(1,factorial+1):
+    if factorial % i == 0:
+        divisor.append(i)
+
+# print(divisor)
+
+ans = len(divisor) % (10 ** 9 + 7)
+
+print(ans)
