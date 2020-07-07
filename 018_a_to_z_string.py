f0263f00825e92b3d5a6fa5bb38673d781f07746
shusuke0812 <shu.ota0812@gmail.com>
2020-07-07 13:13:06 +0900

    [add]解答を追加

--- 018_a_to_z_string.py
+++ 018_a_to_z_string.py
@@ -14,3 +14,35 @@ s
 出力
 答えを出力
 """
+
+# 考え：文字列sを先頭から見ていき、最初にAが来る要素番号、最後にZが来る要素番号がわかれば良い
+# 方針：Aは文字列を左から探索して最初に検出した要素番号、Zは文字列を右から探索して最初に検出した要素番号
+
+s = input()
+s_start = 0
+s_end = 0
+count = 0
+
+# 文字列sを右から探索できるように逆順に変換する
+s_reverse = s[::-1]
+# print(s_reverse)
+
+for i in s:
+    if i == 'A':
+        s_start = count
+        break
+    count += 1
+
+# カウンタcountを逆順用に変更（降順）
+count = len(s) - 1
+
+for j in s_reverse:
+    if j == 'Z':
+        s_end = count
+        break
+    count -= 1
+
+ans = s_end - s_start + 1
+print(ans)
+
+#EOF
