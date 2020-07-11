9ea77fbf5e6bb65c6e07eb6a0397ca9ebcbeb71e
shusuke0812 <shu.ota0812@gmail.com>
2020-07-12 01:02:19 +0900

    [add]解答を追加

--- 021_template_matching.py
+++ 021_template_matching.py
@@ -43,10 +43,13 @@ for j in range(m):
 # 考え：同じ文字列の並びがあるか各行、各列で見る
 # 方針：画像の左上から各行ごとに先頭からAとBの文字列を比較していきマッチするかどうかを調べる
 
+"""ボツ案
 a_pix = []
 b_pix = []
 a_temp = []
 b_temp = []
+
+judge = ''
 
 for i in a:
     for i_pix in i:
@@ -60,5 +63,23 @@ for j in b:
     b_pix.append(b_temp)
     b_temp = []
 
-print(a_pix)
-print(b_pix)
+# print(a_pix)
+# print(b_pix)
+"""
+
+for i in range(n-m+1):
+    for j in range(n-m+1):
+
+
+        judge = 'Yes'
+        for k in range(m):
+            for l in range(m):
+                # 横にずらして文字列を比較
+                if a[i+k][j+l] != b[k][l]:
+                    judge = 'No'
+
+        if judge == 'Yes':
+            print('Yes')
+            exit()
+
+print('No')
